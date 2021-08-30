db44237744af6d8dd1fc2d284806f9f77cf342c9
John Lee <john.lee@wfs.aero>
2021-08-30 12:58:12 -0400

    Initial commit

---
+++ Models/Server.cs
@@ -0,0 +1,9 @@
+namespace Advantage.API.Models
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public bool IsOnline { get; set; }
+    }
+}
